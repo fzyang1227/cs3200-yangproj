47eb4fe07924e61d0cc24e23f45c49c82f0374f3
fzyang1227 <62119227+fzyang1227@users.noreply.github.com>
2020-10-12 00:38:45 -0400

    added hello string and object end points

---
+++ src/main/java/com/example/cs3200yang/hello/hello.java
@@ -0,0 +1,14 @@
+package com.example.cs3200yang.hello;
+
+public class hello {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
